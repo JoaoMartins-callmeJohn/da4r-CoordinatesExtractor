02f6511b1a88e221238786f7c6e8c560c649ad7f
JoaoMartins-Forge <75683555+JoaoMartins-callmeJohn@users.noreply.github.com>
2022-12-14 17:28:58 -0300

    first commit

---
+++ ClassLibrary1/CoordinatesExtractor.cs
@@ -0,0 +1,136 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CoordinatesExtractor
+{
+	[Regeneration(RegenerationOption.Manual)]
+	[Transaction(TransactionMode.Manual)]
+	public class CoordinatesExtractor : IExternalDBApplication
+	{
+		public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+		{
+			return ExternalDBApplicationResult.Succeeded;
+		}
+
+		public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+		{
+			DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+			return ExternalDBApplicationResult.Succeeded;
+		}
+
+		private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+		{
+			e.Succeeded = true;
+			ExtractModelCoordinates(e.DesignAutomationData);
+		}
+
+		private void ExtractModelCoordinates(DesignAutomationData designAutomationData)
+		{
+			if (designAutomationData == null) throw new ArgumentNullException(nameof(designAutomationData));
+
+			Application rvtApp = designAutomationData.RevitApp;
+			if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
+
+			string modelPath = designAutomationData.FilePath;
+			if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
+
+			Document doc = designAutomationData.RevitDoc;
+			if (doc == null) throw new InvalidOperationException("Could not open document.");
+
+			List<Coordinates> coordinates = GetCoordinates("testacciona.csv");
+
+			InputParams inputParameters = JsonConvert.DeserializeObject<InputParams>(File.ReadAllText("params.json"));
+
+			dynamic urnResult = new JObject();
+			try
+			{
+				//urnResult.projectBasePoint = BasePoint.GetProjectBasePoint(doc);
+				BasePoint pbp = BasePoint.GetProjectBasePoint(doc);
+				Console.WriteLine($"Project base point acquired!");
+				//urnResult.surveyPoint = BasePoint.GetSurveyPoint(doc);
+				BasePoint sp = BasePoint.GetSurveyPoint(doc);
+				Console.WriteLine($"Survey point acquired!");
+				//urnResult.angleToTrueNorth = doc.ActiveProjectLocation.GetProjectPosition(XYZ.Zero).Angle;
+				double pp = doc.ActiveProjectLocation.GetProjectPosition(XYZ.Zero).Angle;
+				Console.WriteLine($"True north angle acquired!");
+
+				try
+				{
+					Coordinates correctCoordinates = coordinates.Find(c => c.basePoint.);
+				}
+				catch (Exception)
+				{
+
+					throw;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+
+			// save all to a .json file
+			using (StreamWriter file = File.CreateText("result.json"))
+			using (JsonTextWriter writer = new JsonTextWriter(file))
+			{
+				urnResult.WriteTo(writer);
+			}
+		}
+
+		private static List<Coordinates> GetCoordinates(string fileName)
+		{
+			List<Coordinates> coordinates = new List<Coordinates>();
+			using (var reader = new StreamReader(fileName))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					var values = line.Split(',');
+
+					XYZ projectBasePoint = new XYZ(
+						double.Parse(values[1]),
+						double.Parse(values[2]),
+						double.Parse(values[3])
+					);
+
+					XYZ surveyPoint = new XYZ(
+						double.Parse(values[5]),
+						double.Parse(values[6]),
+						double.Parse(values[7])
+					);
+
+					coordinates.Add(new Coordinates()
+					{
+						code = values[0],
+						angle = double.Parse(values[4]),
+						basePoint = projectBasePoint,
+						surveyPoint = surveyPoint
+					});
+				}
+			}
+			return coordinates;
+		}
+	}
+
+	internal class InputParams
+	{
+		public string userId { get; set; }
+		public string versionUrn { get; set; }
+		public string projectId { get; set; }
+	}
+
+	public class Coordinates
+	{
+		public XYZ basePoint { get; set; }
+		public XYZ surveyPoint { get; set; }
+		public double angle { get; set; }
+		public string code { get; set; }
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# da4r-CoordinatesExtractor
+# Revit Coordinates Extractor bundle for Forge Design Automation
+
+[![Design Automation](https://img.shields.io/badge/Design%20Automation-v3-green.svg)](http://developer.autodesk.com/)
+
+![Windows](https://img.shields.io/badge/Plugins-Windows-lightgrey.svg)
+![.NET](https://img.shields.io/badge/.NET%20Framework-4.8-blue.svg)
+[![Revit-2023](https://img.shields.io/badge/Revit-2022-lightgrey.svg)](http://autodesk.com/revit)
+
+
+# Description
+
+This sample demonstrates a way to retrieve coordinates (project base point, survey point and true north) of materials for a Revit project
+
+# Development Setup
+
+## Prerequisites
+
+1. **Forge Account**: Learn how to create a Forge Account, activate subscription and create an app at [this tutorial](http://learnforge.autodesk.io/#/account/). 
+2. **Visual Studio 2022** (Windows).
+3. **Revit 2023**: required to compile changes into the plugin
+
+## Design Automation Setup
+
+### Activity example
+
+```json
+{
+  "id": "RevitMaterialsExtractorActivity",
+  "commandLine": [
+    "$(engine.path)\\\\revitcoreconsole.exe /i \"$(args[inputFile].path)\" /al \"$(appbundles[CoordinatesExtractor].path)\""
+  ],
+  "parameters": {
+    "inputFile": {
+      "verb": "get",
+      "description": "Input Revit File",
+      "required": true,
+      "localName": "$(inputFile)"
+    },
+    "result": {
+      "zip": false,
+      "verb": "put",
+      "description": "Extracted materials from model",
+      "localName": "result.json"
+    }
+  },
+  "engine": "Autodesk.Revit+2023",
+  "appbundles": [
+    "Autodesk.CoordinatesExtractor+dev"
+  ],
+  "description": "Activity of Revit Coordinates extractor"
+}
+```
+
+### Workitem example
+
+```json
+{
+  "activityId": "ID OF THE ACTIVITY",
+  "arguments": {
+    "inputFile": {
+      "url": "URL TO DOWNLOAD THE INPUT FILE (BIM360/ACC)",
+      "Headers": {
+        "Authorization": "Bearer TOKEN"
+      }
+    },
+    "result": {
+      "verb": "post",
+      "url": "URL TO UPLOAD THE RESULT",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onProgress": {
+      "verb": "post",
+      "url": "URL TO RECEIVE PROGRESS INFORMATION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onComplete": {
+      "verb": "post",
+      "url": "URL TO RECEIVE STATUS AFTER JOB EXECUTION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+}
+```
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+## Written by
+
+João Martins [@JooPaulodeOrne2](http://twitter.com/JooPaulodeOrne2), [APS Partner Development](http://aps.autodesk.com)
